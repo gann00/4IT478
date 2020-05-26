7d767bdd922eea549273c8f936ab8c1851225594
ZarnovskaErika <zare02@vse.cz>
2020-05-26 13:27:55 +0200

    InvalidLogin done

---
+++ src/test/java/cz/project/InvalidLogin.java
@@ -0,0 +1,64 @@
+package cz.project;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class InvalidLogin {
+    private ChromeDriver driver;
+
+    @Before
+    public void setup() {
+        ChromeOptions cho = new ChromeOptions();
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+//        ChromeDriverService service = new ChromeDriverService()
+        driver = new ChromeDriver(cho);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+//        driver.close();
+    }
+
+    @Test
+    public void InvalidLoginUsingInvalidUsernameAndInvalidPassword_userStaysAtLoginPage() {
+        // given
+        driver.get("http://digitalnizena.cz/church/");
+
+        // when
+        WebElement usernameInput = driver.findElement(By.id("UserBox"));
+        usernameInput.sendKeys("invalidlogin");
+        WebElement passwordInput = driver.findElement(By.id("PasswordBox"));
+        passwordInput.sendKeys("invalidpass");
+        WebElement loginButton = driver.findElement(By.className("btn-primary"));
+        loginButton.click();
+        // then
+        // validation error exists
+        WebElement errorMessageSpan = driver.findElement(By.id("spanMessage"));
+        Assert.assertEquals("Invalid credentials", errorMessageSpan.getText());
+    }
+
+
+}
