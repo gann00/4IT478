01142275f5a51b312303bcfc227d8c52580e6a41
Natalia Hancsics <gann00@vse.cz>
2020-05-26 10:15:35 +0200

    Adding project files

--- src/test/java/cz/project/AppTest.java
+++ src/test/java/cz/project/AppTest.java
@@ -1,6 +1,5 @@
 package cz.project;
 
-import cz.churchcrm.testframework.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -57,6 +56,8 @@ public class AppTest {
         WebElement errorMessageSpan = driver.findElement(By.id("spanMessage"));
         Assert.assertEquals("Invalid credentials", errorMessageSpan.getText());
     }
+
+
     @Test
     public void shouldLoginUsingValidCredentials() {
         // given
@@ -94,11 +95,57 @@ public class AppTest {
 
         // THEN newly added deposit should be shown in deposits table grid
 
-        driver.quit();
+    // option2 - use custom "expected condition" of WebDriver framework
+    WebDriverWait wait = new WebDriverWait(driver, 2);     // timeout after 2 seconds
+        wait.until(new ExpectedCondition<Boolean>() {
+        @Override
+        public Boolean apply(WebDriver webDriver) {
+            // each time, we try to get the very first row from table grid and check, if contains the last record
+
+            List<WebElement> depositRows = driver.findElements(By.cssSelector("#depositsTable_wrapper #depositsTable tbody tr"));
+            WebElement firstRow = depositRows.get(0);
+            String innerHTML = firstRow.getAttribute("innerHTML");
+
+            if (innerHTML.contains(uuid)) {
+                Assert.assertTrue(innerHTML.contains("10-30-18"));    // beware, different date format in table grid vs. input field
+                Assert.assertTrue(innerHTML.contains(depositComment));
+                return true;     // expected condition is met
+            } else {
+                return false;    // selenium webdriver will continue polling the DOM each 500ms and check the expected condition by calling method apply(webDriver) again
+            }
+        }
+    }
+        );}
+
+    public void deleteDeposits() throws InterruptedException {
+        shouldLoginUsingValidCredentials();
+
+        driver.get("http://digitalnizena.cz/church/FindDepositSlip.php");
+
+        Thread.sleep(1000);
+
+        List<WebElement> depositRows = driver.findElements(By.cssSelector("#depositsTable tbody tr"));
+
+        for (WebElement row : depositRows) {
+            row.click();
+        }
+
+        WebElement deleteButton = driver.findElement(By.cssSelector("#deleteSelectedRows"));
+        deleteButton.click();
+
+        //TODO compare this WebElement confirmDeleteButton = driver.findElement(By.cssSelector(".modal-dialog .btn-primary"));
+        WebElement confirmDeleteButton = driver.findElement(By.cssSelector(".modal-content > .modal-footer .btn-primary"));
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.until(ExpectedConditions.visibilityOf(confirmDeleteButton));
+        confirmDeleteButton.click();
+
+       // actually the application behaves incorrect => when delete all rows, Delete button should be disabled
+        // we have our test correct, so it good that test fails!
+        Assert.assertFalse(deleteButton.isEnabled());
     }
 
     @Test
-    public void userPepaExistsInSystem_whenUserLogsWithValidPassword_thenUserIsLoggedIntoDashboard() {
+    public void userExistsInSystem_whenUserLogsWithValidPassword_thenUserIsLoggedIntoDashboard() {
         // Given
         driver.get("http://digitalnizena.cz/church/");
 
